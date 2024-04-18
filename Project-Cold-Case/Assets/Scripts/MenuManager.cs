6c5f8e7ef86a1ef613d0e68f646d083dfe4070a1
MaxVendely <maxvendely@gmail.com>
2024-04-18 17:01:28 -0400

    Play and Close buttons

--- Project-Cold-Case/Assets/Scripts/MenuManager.cs
+++ Project-Cold-Case/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void LoadScene()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseGame()
     {
-        
+        Application.Quit();
     }
 }
