ee80f510e0047211fadb81d9a716e4625e12f444
MaxVendely <maxvendely@gmail.com>
2024-05-03 02:01:00 -0400

    Intro card has new text inputs

--- Project-Cold-Case/Assets/Scripts/TextPrinter.cs
+++ Project-Cold-Case/Assets/Scripts/TextPrinter.cs
@@ -10,6 +10,10 @@ public class TextPrinter : MonoBehaviour
 
     public TextMeshProUGUI myTMPro;
 
+    private bool isDone1 = false;
+
+    private bool isDone2 = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,31 +23,46 @@ public class TextPrinter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && isDone1)
+        {
+            StartCoroutine(CloseIntro());
+        }
     }
 
     IEnumerator PrintText(string printText)
     {
+        float printSpeed = .05f;
+
         yield return new WaitForSeconds(.5f);
 
         string currentText = "";
 
         for (int i = 0; i < printText.Length; i++)
         {
+            printSpeed = .05f;
+
+            if (Input.GetMouseButton(0))
+            {
+                printSpeed = .025f;
+            }
+
             currentText += printText[i];
 
             myTMPro.text = currentText;
 
-            yield return new WaitForSeconds(.05f);
+            yield return new WaitForSeconds(printSpeed);
         }
 
+        isDone1 = true;
+    }
 
-        yield return new WaitForSeconds(2);
-
+    IEnumerator CloseIntro()
+    {
         myTMPro.text = "";
 
         yield return new WaitForSeconds(.5f);
 
         SceneManager.LoadScene("SampleScene");
     }
+
 }
